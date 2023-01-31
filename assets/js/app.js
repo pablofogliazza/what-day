2cb9212e3740c4b054d3ee7c97ea94377f3866b3
Pablo Fogliazza <hi@pablofogliazza.dev>
2023-01-31 09:48:38 -0300

    Fix minutes to two digits

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,9 @@ setInterval(() => {
     hour = `0${hour}`
   }
   let minutes = date.getMinutes()
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
   let seconds = date.getSeconds()
   if (seconds < 10) {
     seconds = `0${seconds}`
